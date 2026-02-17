0309c80dba188e8381ef87eda6e35f001e6b815e
hiraisrar123 <hiraisrar47@gmail.com>
2026-02-17 19:04:54 +0500

    gura

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+       {/* Logo */}
+        <h1 className="text-xl font-bold text-blue-600">
+          BrandName
+        </h1>
+         {/* Menu */}
+        <ul className="hidden md:flex space-x-6 text-gray-700 font-medium">
+          <li className="hover:text-blue-600 cursor-pointer">Home</li>
+          <li className="hover:text-blue-600 cursor-pointer">Features</li>
+          <li className="hover:text-blue-600 cursor-pointer">Pricing</li>
+          <li className="hover:text-blue-600 cursor-pointer">Contact</li>
+        </ul>
+        {/* Button */}
+        <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+          Sign Up
+        </button>
+      </div>
+    </nav>
   )
 }
 
